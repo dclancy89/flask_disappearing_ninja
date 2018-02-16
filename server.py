ca580705470ec58f179658a4d776e54fd39938aa
Daniel <clancy.daniel@gmail.com>
2018-02-16 12:26:50 -0600

    removed the css folder. not needed. Added routes for ninja and ninja/color

--- server.py
+++ server.py
@@ -5,4 +5,22 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/ninja')
+def ninja():
+	return render_template('ninja.html', img='tmnt.png')
+
+@app.route('/ninja/<color>')
+def ninja_color(color):
+	if color == "blue":
+		img = "leonardo.jpg"
+	elif color == "orange":
+		img = "michelangelo.jpg"
+	elif color == "red":
+		img = "raphael.jpg"
+	elif color == "purple":
+		img = "donatello.jpg"
+	else:
+		img = "notapril.jpg"
+	return render_template('ninja.html', img=img)
+
 app.run(debug=True)
